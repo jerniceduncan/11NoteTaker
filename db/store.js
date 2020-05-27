83debd85206251777dc6a0322adce31eb5425489
Jernice <jerniceduncan@icloud.com>
2020-05-26 18:50:01 -0700

    updated folder structor

--- db/store.js
+++ db/store.js
@@ -2,7 +2,7 @@ const util = require("util");
 const fs = require("fs");
 
 // This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
-const uuidv1 = require("uuid/v1");
+const uuidv1 = require("uuid");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
